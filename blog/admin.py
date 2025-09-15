cd5f213230bae1348b7deaf43213a8ad096ab4c9
User <ninono12@gmail.com>
2025-09-15 21:18:07 +0400

    WIP: Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,23 +1,34 @@
 from django.contrib import admin
 from blog_app.models import BlogPost, BlogPostImage, Author, BannerImage
 
-admin.site.register(BlogPostImage)
-admin.site.register(Author)
-admin.site.register(BannerImage)
+
+@admin.register(BannerImage)
+class BannerImageAdmin(admin.ModelAdmin):
+    raw_id_fields = ('blog_post',)
 
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'age')
+
+
+class BlogPostImageInline(admin.TabularInline):
+    model = BlogPostImage
+    extra = 4
+
+
+@admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
+    inlines = [BlogPostImageInline]
     list_display = ('title', 'active', 'deleted')
     list_filter = ('active', 'deleted')
     search_fields = ('title',)
     ordering = ('-create_date',)
-    #list_per_page = 1
     date_hierarchy = 'create_date'
-    #fields = ('title', 'active', 'deleted')
-    #exclude = ['title',]
-    # filter_vertical = ('authors',)
     filter_horizontal = ('authors',)
 
-
-
-admin.site.register(BlogPost, BlogPostAdmin)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(active=True)
--- blog/models.py
+++ blog/models.py
@@ -1,83 +1,34 @@
-from datetime import date
-
 from django.db import models
-from django.db.models.query import QuerySet
+from django.utils.timezone import now
 
 class Author(models.Model):
-    first_name = models.CharField(verbose_name='სახელი', max_length=100)
-    last_name = models.CharField(verbose_name='გვარი', max_length=100)
-    birth_date = models.DateField(verbose_name='დაბადების თარიღი', null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     @property
     def age(self):
-        if not self.birth_date:  # თუ არ აქვს დაბადების თარიღი
-            return None
-        today = date.today()
-        return today.year - self.birth_date.year - (
-                (today.month, today.day) < (self.birth_date.month, self.birth_date.day)
-        )
-
-    class Meta:
-        verbose_name = "Author"
-        verbose_name_plural = "Authors"
-
-    def get_blog_posts(self) -> QuerySet["BlogPost"]:
-        return self.blog_posts.all()
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
+        from datetime import date
+        if self.birth_date:
+            today = date.today()
+            return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+        return None
 
 class BlogPost(models.Model):
-    title = models.CharField(verbose_name="სათაური", max_length=255)
-    text = models.CharField(verbose_name="ტექსტი")
-    active = models.BooleanField(default=True, verbose_name='აქტიურია')
-    create_date = models.DateTimeField(verbose_name='შექმნის თარიღი', null=True)
-    update_date = models.DateTimeField(verbose_name="განახლების თარიღი", null=True)
-    website = models.URLField(verbose_name='ვებ მისამართი', null=True, blank=True)
-    document = models.FileField(upload_to='blog_document/', null=True, blank=True)
-    authors = models.ManyToManyField('blog_app.Author', related_name='blog_posts', blank=True)
-    deleted = models.BooleanField(verbose_name="წაშლილია", default=False)
-    create_date_date = models.DateTimeField(verbose_name='შექმნის თარიღი', auto_now_add=True)
-
-    class Meta:
-        verbose_name = "Blog Post"
-        verbose_name_plural = "Blog Posts"
-        ordering = ['title']
-        unique_together = [['title', 'text']]
-
-    def get_images(self) -> QuerySet['BlogPostImage']:
-        return BlogPostImage.objects.filter(blog_post=self)
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class BannerImage(models.Model):
-    blog_post = models.OneToOneField(
-        'blog_app.BlogPost',
-        related_name='banner_image',
-        on_delete=models.CASCADE
-    )
-    image = models.ImageField(upload_to='banner_image/', null=True, blank=True)
-
-    class Meta:
-        verbose_name = "Banner Image"
-        verbose_name_plural = "Banner Images"
-        ordering = ['id']
-
-    def __str__(self) -> str:
-        return f'{self.blog_post.title} - {self.id} image'
-
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author)
+    active = models.BooleanField(default=True)
+    deleted = models.BooleanField(default=False)
+    create_date = models.DateTimeField(default=now)
 
 class BlogPostImage(models.Model):
-    image = models.ImageField(upload_to='blog_image/')
-    blog_post = models.ForeignKey('blog_app.BlogPost', on_delete=models.CASCADE, related_name='images')
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to="blog_images/")
 
-    class Meta:
-        verbose_name = "Blog Post Image"
-        verbose_name_plural = "Blog Post Images"
-        ordering = ['id']
-
-    def __str__(self) -> str:
-        return f'{self.blog_post.title} - {self.id} image'
+class BannerImage(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='banners')
+    image = models.ImageField(upload_to="banner_images/", default='banner_images/default.jpg')
